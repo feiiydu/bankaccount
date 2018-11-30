251ac1d386d3959564a9b4b2fca2cfb7a245e3e9
apitsada suan <feiiydu@gmail.com>
2018-11-30 16:07:46 +0700

    update

--- bankaccount/bankaccount.go
+++ bankaccount/bankaccount.go
@@ -1,9 +1,15 @@
 package bankaccount
 
+import "database/sql"
+
 type BankAccount struct {
 	ID        int
 	UserID    string
 	AccountID int
 	Name      string
 	Balance   float64
+}
+
+type Manager struct {
+	DB *sql.DB
 }
