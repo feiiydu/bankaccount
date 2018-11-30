ec03448e4ea03c19ad8cb726157607b08c804340
apitsada suan <feiiydu@gmail.com>
2018-11-30 10:44:07 +0700

    commit

---
+++ bankaccount/bankaccount.go
@@ -0,0 +1,9 @@
+package bankaccount
+
+type BankAccount struct {
+	ID        int
+	UserID    string
+	AccountID int
+	Name      string
+	Balance   float64
+}
---
+++ user/user.go
@@ -0,0 +1,7 @@
+package user
+
+type User struct {
+	ID        int
+	FirstName string
+	LastName  string
+}
